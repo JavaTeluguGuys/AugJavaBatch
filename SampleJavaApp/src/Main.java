9e96fc757253d46ea6b5c10602867a606f0087b5
JavaTeluguGuys <javateluguguys@gmail.com>
2022-09-17 20:33:22 +0530

    New SOP statement added

--- SampleJavaApp/src/Main.java
+++ SampleJavaApp/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("New message is added!");
+		
+		System.out.println("New message is added by Dileep!"); 
     }
 }
